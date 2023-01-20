79196fc6f5db07d00d7ff2e9f2e73601e9df51b9
Graciliano-Barroso <grico484@gmail.com>
2023-01-20 12:20:14 -0400

    feat: Criando as Dicas

--- app/validacao.js
+++ app/validacao.js
@@ -3,10 +3,12 @@ function veruficaSeOChutePossuiUmValorValido(chute) {
 
     if (seOChuteForInvalido(numero)) {
         elementoChute.innerHTML += '<div>Valor inválido</div>';
+        return
     }
 
     if (seONumeroForMaiorOuMenorQueOValorPermitido(numero)) {
         elementoChute.innerHTML += `<div>Valor inválido! Fale um número entre ${menorValor} e ${maiorValor}</div>`;
+        return
     }
 
     if (numero === numeroSecreto) {
@@ -14,6 +16,14 @@ function veruficaSeOChutePossuiUmValorValido(chute) {
             <h2>Você acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
         `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-arrow-down"></i></div>
+        `;
+    } else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-arrow-up"></i></div>
+        `;
     }
 }
 
