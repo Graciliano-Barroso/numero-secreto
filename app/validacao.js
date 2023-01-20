7f9470374d6b794e2ffed4ae8ed629364c86f5b9
Graciliano-Barroso <grico484@gmail.com>
2023-01-20 11:48:26 -0400

    feat: Acertando o Número Secreto

--- app/validacao.js
+++ app/validacao.js
@@ -2,11 +2,18 @@ function veruficaSeOChutePossuiUmValorValido(chute) {
     const numero = +chute;
 
     if (seOChuteForInvalido(numero)) {
-        console.log('valor inválido');
+        elementoChute.innerHTML += '<div>Valor inválido</div>';
     }
 
     if (seONumeroForMaiorOuMenorQueOValorPermitido(numero)) {
-        console.log(`Valor inválido! O número secreto precisa esta entre ${menorValor} e ${maiorValor}`);
+        elementoChute.innerHTML += `<div>Valor inválido! Fale um número entre ${menorValor} e ${maiorValor}</div>`;
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+        `
     }
 }
 
